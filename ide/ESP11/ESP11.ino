20e2a0ec0a1147326dc3520fae90bd83fd156316
Kydinhvan <101254140+Kydinhvan@users.noreply.github.com>
2024-04-15 18:03:08 +0800

    Update ESP11.ino hex work

--- ide/ESP11/ESP11.ino
+++ ide/ESP11/ESP11.ino
@@ -3,6 +3,7 @@
 #include "BLEServer.h"
 #include "BLEBeacon.h"
 #include "esp_sleep.h"
+
 // Define the name of the BLE server
 #define SERVER_NAME "LAPTOP-U0NSBAGO"
 // this is esp11
@@ -10,9 +11,7 @@
 #define BEACON_UUID "12345678-1234-5678-1234-56789abcdef1" // UUID 1 128-Bit
 RTC_DATA_ATTR static uint32_t bootcount; // remember number of boots in RTC Memory
 
-
 #define GPIO_PIN 2
-
 
 const char* message = "#de2323 esp11";
 uint8_t ledR = 39; // ESP32-S3 pin for red LED | GPIO 39 
@@ -22,18 +21,45 @@ uint8_t ledB = 41; // ESP32-S3 pin for blue LED | GPIO 41
 const boolean invert = true; // true: common anode logic '1' | false: common cathode logic '0' 
  
 String hexValue_S = "#005ac8";
-
+//7bffff04
 String lastEightChars = "";
 
 BLEAdvertising *pAdvertising; // BLE Advertisement type
 BLEScan *pBLEScan;            // BLE Scan type
 BLECharacteristic *pCharacteristic; // BLE Characteristic for sending message
 
+void setRGBColor(uint8_t R, uint8_t G, uint8_t B) { 
+    // Print debug information 
+    Serial.println("Setting RGB values:"); 
+    Serial.print("R: "); Serial.println(R); 
+    Serial.print("G: "); Serial.println(G); 
+    Serial.print("B: "); Serial.println(B); 
+ 
+    // Set PWM duty cycle for each color 
+    ledcWrite(1, R); 
+    ledcWrite(2, G); 
+    ledcWrite(3, B); 
+}
+
+void ledSetup() {
+    // Setup PWM channels for each LED pin 
+    ledcAttachPin(ledR, 1); // assign RGB led pins to channels 
+    ledcAttachPin(ledG, 2); 
+    ledcAttachPin(ledB, 3); 
+    
+    // Initialize PWM channels 
+    ledcSetup(1, 12000, 8); // 12 kHz PWM, 8-bit resolution 
+    ledcSetup(2, 12000, 8); 
+    ledcSetup(3, 12000, 8); 
+
+}
+
 class MyAdvertisedDeviceCallbacks : public BLEAdvertisedDeviceCallbacks {
     void onResult(BLEAdvertisedDevice advertisedDevice) {
-        int pinValue = digitalRead(GPIO_PIN);
-        delay(2000);
-        if(pinValue == 0) {
+        
+        // int pinValue = digitalRead(GPIO_PIN);
+        // delay(2000);
+        // if(pinValue == 0) {
           if (advertisedDevice.haveName() && advertisedDevice.getName() == SERVER_NAME) {
               for (int i = 0; i < advertisedDevice.getServiceUUIDCount(); i++) {
                   BLEUUID serviceUUID = advertisedDevice.getServiceUUID(i);
@@ -43,18 +69,40 @@ class MyAdvertisedDeviceCallbacks : public BLEAdvertisedDeviceCallbacks {
                   if (uuidStr.indexOf("a07498ca-ad5b-474e-940d-16f") != -1 && newLastEightChars != lastEightChars) {
                       lastEightChars = newLastEightChars;
                       Serial.println(lastEightChars); // when this is printed, it means the user init new color on the website
-                  } else {Serial.println("Nth change");}
-              }
-          } 
-        } else if (pinValue == 1) { // switch back later 1
+                      // Convert lastEightChars to std::string for manipulation
+                      std::string stdLastEightChars = lastEightChars.c_str();
+                      stdLastEightChars.erase(stdLastEightChars.size() - 2);
+                      lastEightChars = stdLastEightChars.c_str();
+                      hexValue_S = "#" + lastEightChars;
+                      uint32_t hexValue = (uint32_t) strtol(hexValue_S.substring(1).c_str(), NULL, 16); // Skip the '#' character 
+            
+                      // Extract RGB components 
+                      uint8_t R = (hexValue >> 16) ; // Red component 
+                      uint8_t G = (hexValue >> 8);  // Green component 
+                      uint8_t B = hexValue ;         // Blue component 
+                      
+                      // Set LED color based on RGB values 
+                      setRGBColor(R, G, B); 
+                      
+                      // Print the hex value and the corresponding RGB components 
+                      Serial.print("Hex Value: "); 
+                      Serial.print(hexValue_S); 
+                      Serial.print("\nRGB: "); 
+                      
+                      delay(2000);}
+                    }
+                  }
+        // } 
+        // else if (pinValue == 1) { // switch back later 1
             if (advertisedDevice.getName().find("esp11") != std::string::npos) { // to filter the unwanted signals
                           std::string namePrefix = advertisedDevice.getName().substr(0, 7); // get hex
                           Serial.println(namePrefix.c_str());
                           Serial.println(advertisedDevice.getRSSI()); //printing distance
             }
-        }
+        // }
     }
 };
+
 
 
 void scanningSetup() {
@@ -112,6 +160,8 @@ void characteristicSetup() {
 }
 
 void setup() {
+    ledSetup();
+
     Serial.begin(115200);
 
     pinMode(GPIO_PIN, INPUT);
