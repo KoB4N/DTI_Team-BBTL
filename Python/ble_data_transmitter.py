fe33a2139b1c8d2559ba111083f72f7f07e00e44
Kydinhvan <dinhvanky2042002@gmail.com>
2024-04-19 12:05:42 +0800

    added new ide + pdf

--- Python/ble_data_transmitter.py
+++ Python/ble_data_transmitter.py
@@ -2,9 +2,9 @@ import sys
 import logging
 import asyncio
 import threading
+import json
 
-from typing import Any, Union
-
+from typing import Union
 from bless import (
     BlessServer,
     BlessGATTCharacteristic,
@@ -12,17 +12,9 @@ from bless import (
     GATTAttributePermissions,
 )
 
-import json
-############## & C:/Users/dinhv/anaconda3/envs/python.exe C:\Users\dinhv\Desktop\dti3\DTI_Team-BBTL\Python\ble_data_transmitter.py
-
-# Open the JSON file and load hex
-with open('output.json', 'r') as f:
-    data = json.load(f)
-output_string = data['output_string']
-
 logging.basicConfig(level=logging.DEBUG)
 logger = logging.getLogger(name=__name__)
-
+## & C:/Users/dinhv/anaconda3/envs/python.exe C:\Users\dinhv\Desktop\dti3\DTI_Team-BBTL\Python\ble_data_transmitter.py
 # NOTE: Some systems require different synchronization methods.
 trigger: Union[asyncio.Event, threading.Event]
 if sys.platform in ["darwin", "win32"]:
@@ -30,55 +22,60 @@ if sys.platform in ["darwin", "win32"]:
 else:
     trigger = asyncio.Event()
 
-my_service_uuid = "A07498CA-AD5B-474E-940D-16F1FBE7E8CD"
-if output_string != None:
-    my_service_uuid = my_service_uuid[:-8] + output_string[1:]
-    print(my_service_uuid)
-else:
-    my_service_uuid = my_service_uuid[:-8] + "ffffff04"
-
 def read_request(characteristic: BlessGATTCharacteristic, **kwargs) -> bytearray:
     logger.debug(f"Reading {characteristic.value}")
     return characteristic.value
 
-
 async def run(loop):
     trigger.clear()
-    # Instantiate the server
-    my_service_name = "Test Service"
-    server = BlessServer(name=my_service_name, loop=loop)
-    server.read_request_func = read_request
+    while True:
+        # Open the JSON file and load hex
+        with open('output.json', 'r') as f:
+            data = json.load(f)
+        output_string = data.get('output_string')
+        
+        my_service_uuid = "A07498CA-AD5B-474E-940D-16F1FBE7E8CD"
+        if output_string is not None:
+            # my_service_uuid = my_service_uuid[:-8] + "00ff0002"
+            my_service_uuid = my_service_uuid[:-8] + output_string[1:]
+            logger.debug(f"New UUID: {my_service_uuid}")
+        
+        # Instantiate the server
+        my_service_name = "Test Service"    
+        server = BlessServer(name=my_service_name, loop=loop)
+        server.read_request_func = read_request
 
-    # Add Service
-    await server.add_new_service(my_service_uuid)
+        # Add Service
+        await server.add_new_service(my_service_uuid)
 
-    # Add a Characteristic to the service
-    my_char_uuid = "51FF12BB-3ED8-46E5-B4F9-D64E2FEC021B"
-    char_flags = (
-        GATTCharacteristicProperties.read
-        | GATTCharacteristicProperties.write
-        | GATTCharacteristicProperties.indicate
-    )
-    permissions = GATTAttributePermissions.readable | GATTAttributePermissions.writeable
-    await server.add_new_characteristic(
-        my_service_uuid, my_char_uuid, char_flags, None, permissions
-    )
+        # Add a Characteristic to the service
+        my_char_uuid = "51FF12BB-3ED8-46E5-B4F9-D64E2FEC021B"
+        char_flags = (
+            GATTCharacteristicProperties.read
+            | GATTCharacteristicProperties.write
+            | GATTCharacteristicProperties.indicate
+        )
+        permissions = GATTAttributePermissions.readable | GATTAttributePermissions.writeable
+        await server.add_new_characteristic(
+            my_service_uuid, my_char_uuid, char_flags, None, permissions
+        )
 
-    logger.debug(server.get_characteristic(my_char_uuid))
-    await server.start()
-    logger.debug("Advertising")
-    if trigger.__module__ == "threading":
-        trigger.wait()
-    else:
-        await trigger.wait()
+        # logger.debug(server.get_characteristic(my_char_uuid))
+        await server.start()
+        # logger.debug("Advertising")
+        # if trigger.__module__ == "threading":
+        #     trigger.wait()
+        # else:
+        #     await trigger.wait()
 
-    await asyncio.sleep(2)
-    logger.debug("Updating")
-    server.get_characteristic(my_char_uuid)
-    server.update_value(my_service_uuid, "51FF12BB-3ED8-46E5-B4F9-D64E2FEC021B")
-    await asyncio.sleep(5)
-    await server.stop()
+        await asyncio.sleep(4)
+        logger.debug("Updating")
+        server.get_characteristic(my_char_uuid)
+        server.update_value(my_service_uuid, my_char_uuid)
+        await asyncio.sleep(1)
+        await server.stop()
 
+        await asyncio.sleep(1)  # Wait before rechecking the JSON file
 
 loop = asyncio.get_event_loop()
 loop.run_until_complete(run(loop))
--- ide/ESP11/ESP11.ino
+++ ide/ESP11/ESP11.ino
@@ -3,70 +3,210 @@
 #include "BLEServer.h"
 #include "BLEBeacon.h"
 #include "esp_sleep.h"
+
 // Define the name of the BLE server
 #define SERVER_NAME "LAPTOP-U0NSBAGO"
+
 // this is esp11
 #define GPIO_DEEP_SLEEP_DURATION 10  // sleep x seconds and then wake up
 #define BEACON_UUID "12345678-1234-5678-1234-56789abcdef1" // UUID 1 128-Bit
 RTC_DATA_ATTR static uint32_t bootcount; // remember number of boots in RTC Memory
 
-
 #define GPIO_PIN 2
 
 
-const char* message = "#de2323 esp11";
+int counter = -1; /////// set 0 for set 1, -1 for set 2
+
+const float alpha = 0.2; // Smoothing factor, adjust as needed
+
+// Initialize smoothed RSSI value
+float smoothedRSSI = 0;
+
+
 uint8_t ledR = 39; // ESP32-S3 pin for red LED | GPIO 39 
 uint8_t ledG = 40; // ESP32-S3 pin for green LED | GPIO 40 
 uint8_t ledB = 41; // ESP32-S3 pin for blue LED | GPIO 41 
- 
-const boolean invert = true; // true: common anode logic '1' | false: common cathode logic '0' 
- 
-String hexValue_S = "#005ac8";
 
+const boolean invert = true; 
+ 
+String hexValue_S = "";
+//7bffff04
 String lastEightChars = "";
+String oldlastEightChars = "";
+
 
 BLEAdvertising *pAdvertising; // BLE Advertisement type
 BLEScan *pBLEScan;            // BLE Scan type
 BLECharacteristic *pCharacteristic; // BLE Characteristic for sending message
 
+void setRGBColor(uint8_t R, uint8_t G, uint8_t B) { 
+    // Print debug information 
+    // Serial.println("Setting RGB values:"); 
+    Serial.print("R: "); Serial.println(R); 
+    Serial.print("G: "); Serial.println(G); 
+    Serial.print("B: "); Serial.println(B);
+ 
+    // Set PWM duty cycle for each color 
+    ledcWrite(1, R); 
+    ledcWrite(2, G); 
+    ledcWrite(3, B); 
+}
+
+void ledSetup() {
+    // Setup PWM channels for each LED pin 
+    ledcAttachPin(ledR, 1); // assign RGB led pins to channels 
+    ledcAttachPin(ledG, 2); 
+    ledcAttachPin(ledB, 3); 
+    
+    // Initialize PWM channels 
+    ledcSetup(1, 12000, 8); // 12 kHz PWM, 8-bit resolution 
+    ledcSetup(2, 12000, 8); 
+    ledcSetup(3, 12000, 8); 
+
+}
+
+void extractRGB(String hexColor, uint8_t& red, uint8_t& green, uint8_t& blue) {
+    // Convert hex color string to uint32_t
+    uint32_t colorValue = (uint32_t)strtol(hexColor.c_str(), NULL, 16);
+
+    // Extract RGB components
+    red = (colorValue >> 16) & 0xFF;
+    green = (colorValue >> 8) & 0xFF;
+    blue = colorValue & 0xFF;
+}
+
 class MyAdvertisedDeviceCallbacks : public BLEAdvertisedDeviceCallbacks {
     void onResult(BLEAdvertisedDevice advertisedDevice) {
+        
         int pinValue = digitalRead(GPIO_PIN);
-        delay(2000);
-        if(pinValue == 0) {
-          if (advertisedDevice.haveName() && advertisedDevice.getName() == SERVER_NAME) {
-              for (int i = 0; i < advertisedDevice.getServiceUUIDCount(); i++) {
-                  BLEUUID serviceUUID = advertisedDevice.getServiceUUID(i);
-                  String uuidStr = serviceUUID.toString().c_str();
-                  // Extract the last 8 characters of the UUID
-                  String newLastEightChars = uuidStr.substring(28);
-                  if (uuidStr.indexOf("a07498ca-ad5b-474e-940d-16f") != -1 && newLastEightChars != lastEightChars) {
-                      lastEightChars = newLastEightChars;
-                      Serial.println(lastEightChars); // when this is printed, it means the user init new color on the website
-                  } else {Serial.println("Nth change");}
-              }
-          } 
-        } else if (pinValue == 1) { // switch back later 1
-            if (advertisedDevice.getName().find("esp11") != std::string::npos) { // to filter the unwanted signals
-                          std::string namePrefix = advertisedDevice.getName().substr(0, 7); // get hex
-                          Serial.println(namePrefix.c_str());
-                          Serial.println(advertisedDevice.getRSSI()); //printing distance
+        // Serial.println(pinValue);
+        
+        if(pinValue == 0) {  
+          // if (true){
+            if (advertisedDevice.haveName() && advertisedDevice.getName() == SERVER_NAME) {
+                for (int i = 0; i < advertisedDevice.getServiceUUIDCount(); i++) {
+                    BLEUUID serviceUUID = advertisedDevice.getServiceUUID(i);
+                    String uuidStr = serviceUUID.toString().c_str();
+                    // Extract the last 8 characters of the UUID
+                    String newLastEightChars = uuidStr.substring(28);
+                    
+                    // Serial.print("oldLastEightChars: ");
+                    // Serial.println(oldlastEightChars.c_str());
+                    
+                    if (uuidStr.indexOf("a07498ca-ad5b-474e-940d-16f") != -1 && newLastEightChars != oldlastEightChars) {
+                        
+                        oldlastEightChars = newLastEightChars;
+                        String hexuuid = newLastEightChars.substring(0, 6);
+                        String modeuuid = newLastEightChars.substring(6);
+                        // if (modeuuid== "02"){
+                        //   counter++;
+                        // }
+
+                        // Serial.print("Counter:");
+                        // Serial.println(counter);
+                        Serial.print("newLastEightChars: ");
+                        Serial.println(newLastEightChars.c_str());
+                        
+                        // message =  hexuuid + "esp"+ modeuuid;
+                        
+                        // when this is printed, it means the user init new color on the website
+                        // Convert lastEightChars to std::string for manipulation
+                        std::string stdLastEightChars = oldlastEightChars.c_str();
+                        stdLastEightChars.erase(stdLastEightChars.size() - 2);
+                        lastEightChars = stdLastEightChars.c_str();
+                        hexValue_S = "#" + lastEightChars;
+                        uint32_t hexValue = (uint32_t)strtol(hexValue_S.substring(1).c_str(), NULL, 16); // Skip the '#' character 
+
+                        // Extract RGB components 
+                        uint8_t R = (hexValue >> 16) ; // Red component 
+                        uint8_t G = (hexValue >> 8);  // Green component 
+                        uint8_t B = hexValue ;         // Blue component 
+
+                        if (modeuuid== "04"){
+                          // counter = 2;
+                        hexValue_S = "#" + lastEightChars;
+                        uint32_t hexValue = (uint32_t)strtol(hexValue_S.substring(1).c_str(), NULL, 16); // Skip the '#' character 
+                        uint8_t R = (hexValue >> 16) ; // Red component 
+                        uint8_t G = (hexValue >> 8);  // Green component 
+                        uint8_t B = hexValue ;         // Blue component 
+                        setRGBColor(R, G, B);
+                        }
+                        if (modeuuid== "02"){
+                          // counter = 2;
+                          hexValue_S = "#" + lastEightChars;
+                          uint32_t hexValue = (uint32_t)strtol(hexValue_S.substring(1).c_str(), NULL, 16); // Skip the '#' character 
+                          uint8_t R = (hexValue >> 16) ; // Red component 
+                          uint8_t G = (hexValue >> 8);  // Green component 
+                          uint8_t B = hexValue ;         // Blue component 
+                          setRGBColor(R, G, B);
+
+                        }
+                        // if (counter >= 2) { 
+                        //     setRGBColor(R, G, B);
+                        //     Serial.print("Counter reached:");
+                        //     Serial.println(counter);
+                            
+                        // }
+                        // Set LED color based on RGB values 
+                        
+                        
+                        // Print the hex value and the corresponding RGB components 
+                        // Serial.print("Hex Value: "); 
+                        
+                        // Serial.print("\nRGB: "); 
+                        
+                        
+                    }
+                }
+            } 
+        } 
+        else  { // switch back later 1
+        // if (true){
+             if (advertisedDevice.getName().find("esp3") != std::string::npos ) { // only check with esp3 table set 2
+                Serial.println(advertisedDevice.getName().c_str());
+                int rssi = advertisedDevice.getRSSI(); // Retrieve RSSI value
+                Serial.println(rssi); // Print RSSI
+
+                // Extract RGB components from original color hex string
+                uint8_t originalRed = strtol(hexValue_S.substring(1, 3).c_str(), NULL, 16);
+                uint8_t originalGreen = strtol(hexValue_S.substring(3, 5).c_str(), NULL, 16);
+                uint8_t originalBlue = strtol(hexValue_S.substring(5, 7).c_str(), NULL, 16);
+                // Smooth the RSSI value using EMA
+                smoothedRSSI = alpha * rssi + (1 - alpha) * smoothedRSSI;
+                int darkness_level = map(smoothedRSSI, -60, -30, 10, 1); // Map smoothed RSSI to darkness level (1 to 10)
+
+                // Calculate darker shades
+                int darkerRed = constrain(originalRed * (10 - darkness_level) / 10, 5, 255);
+                int darkerGreen = constrain(originalGreen * (10 - darkness_level) / 10, 5, 255);
+                int darkerBlue = constrain(originalBlue * (10 - darkness_level) / 10, 5, 255);
+
+                // Set LED color
+                // if (counter >= 2) {
+                            
+                  // Serial.print("Counter reached:");
+                  // Serial.println(counter);
+                  setRGBColor(darkerRed, darkerGreen, darkerBlue);
+                  Serial.println(darkerRed );
+                  Serial.println(darkerGreen);
+                  Serial.println(darkerBlue); 
+                            
+                // }
+                
             }
         }
     }
 };
 
-
 void scanningSetup() {
-    BLEDevice::init(message);
+    BLEDevice::init("esp1");
     pBLEScan = BLEDevice::getScan();
     BLEScan* pBLEScan = BLEDevice::getScan();
     pBLEScan->setAdvertisedDeviceCallbacks(new MyAdvertisedDeviceCallbacks());
     pBLEScan->setActiveScan(true);
     
     while (true) {
-        pBLEScan->start(2); // Scan for 10 seconds
-        delay(500); // Wait for 2 seconds
+        pBLEScan->start(2); // Scan for x seconds (while scanning, program is delayed)
+        delay(500);// Wait for 2 seconds
     }
 }
 
@@ -88,9 +228,11 @@ void setBeacon() {
     pAdvertising->setAdvertisementData(oAdvertisementData);
     pAdvertising->setScanResponseData(oScanResponseData);
 }
+
 void advertisingSetup() {
     // Create the BLE Device for sender mode
-    BLEDevice::init(message);
+    
+    BLEDevice::init("esp1");
     pAdvertising = BLEDevice::getAdvertising();
     BLEDevice::startAdvertising();
     setBeacon();
@@ -112,6 +254,8 @@ void characteristicSetup() {
 }
 
 void setup() {
+    ledSetup();
+
     Serial.begin(115200);
 
     pinMode(GPIO_PIN, INPUT);
